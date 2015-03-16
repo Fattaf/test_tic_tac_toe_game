19cdb719d3a5d98426a6a39a9d512dc0394e85a0
fattaf <andrew.nashkolniy@gmail.com>
2015-03-16 23:09:40 +0200

    Sinatra. Thin. EmSocket. First calling.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'thin'
+gem 'sinatra'
+gem 'sinatra-websocket', '~> 0.3.1'
---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'thin'
+require 'sinatra/base'
+require 'sinatra-websocket'
+
+
+class App < Sinatra::Base
+  set :server, 'thin'
+  set :sockets, []
+
+  get '/' do
+    unless request.websocket?
+      erb :index
+    else
+      request.websocket do |ws|
+
+        ws.onopen do
+          ws.send("Hello World!")
+          settings.sockets << ws
+        end
+
+        ws.onmessage do |msg|
+          EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+        end
+
+        ws.onclose do
+          warn("websocket closed")
+          settings.sockets.delete(ws)
+        end
+
+      end
+    end
+  end
+
+end
+
+App.run! :port => 3000
