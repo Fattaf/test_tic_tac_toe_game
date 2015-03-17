9c1dfc9bac7ad2694f98208bb2301f731b0762ea
fattaf <andrew.nashkolniy@gmail.com>
2015-03-17 23:14:47 +0200

    added test objects by phaser

--- app.rb
+++ app.rb
@@ -1,12 +1,37 @@
 require 'thin'
 require 'sinatra/base'
 require 'sinatra-websocket'
+require 'sinatra/assetpack'
 
 
 class App < Sinatra::Base
   set :server, 'thin'
   set :sockets, []
+  set :root, File.dirname(__FILE__)
 
+  register Sinatra::AssetPack
+
+  assets {
+    # serve '/js',     from: 'assets/js'
+    # serve '/css',    from: 'assets/css'
+    serve '/images', from: 'assets/images'
+
+    # The second parameter defines where the compressed version will be served.
+    # (Note: that parameter is optional, AssetPack will figure it out.)
+    # js :app, '/js/app.js', [
+    #   '/js/**/*.js',
+    #   '/js/**/*.js'
+    # ]
+
+    # css :application, '/css/application.css', [
+    #   '/css/screen.css'
+    # ]
+
+    # js_compression  :jsmin
+    # css_compression :simple
+  }
+
+  # routes
   get '/' do
     unless request.websocket?
       erb :index
